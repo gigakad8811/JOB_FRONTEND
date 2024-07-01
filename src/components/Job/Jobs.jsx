bef5f3b2f515f532bf30cfa26fad2296aeb6ea21
DEBASHIS DEY <ddey23006@gmail.com>
2024-07-01 21:50:07 +0530

    commit

--- src/components/Job/Jobs.jsx
+++ src/components/Job/Jobs.jsx
@@ -41,7 +41,7 @@ const Jobs = () => {
                   <p>Country: {element.country}</p>
                   <p>City: {element.city}</p>
                   <p>Location: {element.location}</p>
-                  <p>Salary: {element.salary}</p>
+                  <p>Salary: {"click 'Job Details' for more details "}</p>
                   <Link to={`/job/${element._id}`}>Job Details</Link>
                 </div>
               );
